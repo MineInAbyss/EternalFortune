79b10de499bc14042a1ccaf3738a5cb4b4ab3c47
Scyu_ <scyu@scyu.dev>
2021-08-20 22:31:02 +0100

    Add kotlin jvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,12 @@ dependencies {
 	slim("org.jetbrains.exposed:exposed-core:$exposedVersion") { isTransitive = false }
 	slim("org.jetbrains.exposed:exposed-dao:$exposedVersion") { isTransitive = false }
 	slim("org.jetbrains.exposed:exposed-jdbc:$exposedVersion") { isTransitive = false }
-	slim("org.jetbrains.exposed:exposed-java-time:$exposedVersion") {isTransitive = false }
+	slim("org.jetbrains.exposed:exposed-java-time:$exposedVersion") { isTransitive = false }
 	// Sqlite
 	slim("org.xerial:sqlite-jdbc:3.30.1")
+
+	// Kotlin JVM
+	slim(kotlin("stdlib-jdk8"))
 
 	// Shaded
 	implementation("com.mineinabyss:idofront:1.17.1-0.6.23")
