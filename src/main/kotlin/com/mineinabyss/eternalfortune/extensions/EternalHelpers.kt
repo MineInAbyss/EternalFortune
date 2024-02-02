076c6add11b4ab46329453a257ddf40e490ecc57
Boy <sivertpaulsen2@gmail.com>
2024-02-02 17:36:50 +0100

    refactor: remove offlinePDC for idofront impl

--- src/main/kotlin/com/mineinabyss/eternalfortune/extensions/EternalHelpers.kt
+++ src/main/kotlin/com/mineinabyss/eternalfortune/extensions/EternalHelpers.kt
@@ -23,6 +23,8 @@ import com.mineinabyss.idofront.messaging.logError
 import com.mineinabyss.idofront.messaging.success
 import com.mineinabyss.idofront.messaging.warn
 import com.mineinabyss.idofront.nms.nbt.WrappedPDC
+import com.mineinabyss.idofront.nms.nbt.getOfflinePDC
+import com.mineinabyss.idofront.nms.nbt.saveOfflinePDC
 import com.mineinabyss.idofront.textcomponents.miniMsg
 import com.mineinabyss.protocolburrito.dsl.sendTo
 import dev.triumphteam.gui.guis.Gui
@@ -161,50 +163,6 @@ val Player.hasGraves
         ?: false
 val Player.playerGraves get() = toGearyOrNull()?.get<PlayerGraves>()
 fun PlayerGraves.size() = graveUuids.size
-
-/**
- * Gets the PlayerData from file for this UUID.
- */
-internal fun UUID.getOfflinePlayerData(): CompoundTag? =
-    (Bukkit.getServer() as CraftServer).handle.playerIo.getPlayerData(this.toString())
-
-/**
- * Gets a copy of the WrappedPDC for this OfflinePlayer.
- * Care should be taken to ensure that the player is not online when this is called.
- */
-fun OfflinePlayer.getOfflinePDC(): WrappedPDC? {
-    if (isOnline) return null
-    val baseTag = uniqueId.getOfflinePlayerData()?.getCompound("BukkitValues") ?: return null
-    return WrappedPDC(baseTag)
-}
-
-/**
- * Saves the given WrappedPDC to the OfflinePlayer's PlayerData file.
- * Care should be taken to ensure that the player is not online when this is called.
- * @return true if successful, false otherwise.
- */
-fun OfflinePlayer.saveOfflinePDC(pdc: WrappedPDC): Boolean {
-    if (isOnline) return false
-    val worldNBTStorage = (Bukkit.getServer() as CraftServer).server.playerDataStorage
-    val tempFile = File(worldNBTStorage.playerDir, "$uniqueId.dat.tmp")
-    val playerFile = File(worldNBTStorage.playerDir, "$uniqueId.dat")
-
-    val mainPDc = uniqueId.getOfflinePlayerData() ?: return false
-    mainPDc.put("BukkitValues", pdc.compoundTag) ?: return false
-    runCatching {
-        Files.newOutputStream(tempFile.toPath()).use { outStream ->
-            NbtIo.writeCompressed(mainPDc, outStream)
-            if (playerFile.exists() && !playerFile.delete()) logError("Failed to delete player file $uniqueId")
-            if (!tempFile.renameTo(playerFile)) logError("Failed to rename player file $uniqueId")
-        }
-    }.onFailure {
-        logError("Failed to save player file $uniqueId")
-        it.printStackTrace()
-        return false
-    }
-    return true
-}
-
 
 fun Location.ensureWorldIsLoaded() {
     if (Bukkit.isTickingWorlds()) ensureWorldIsLoaded()
